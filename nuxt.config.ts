40ee25c69a0db5dd93cb18b7f3c9061546210460
Pablo <pablo.alcalde.garcia@gmail.com>
2025-02-22 13:53:26 +0100

    error page try

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,16 @@
-// nuxt.config.ts
 export default defineNuxtConfig({
   app: {
     head: {
       link: [
         { rel: 'icon', href: '/favicon.ico' },
         { rel: 'icon', type: 'image/png', href: '/favicon.png', sizes: '32x32' },
+        { rel: 'icon', type: 'image/png', href: '/favicon-16x16.png', sizes: '16x16' },
+        { rel: 'icon', type: 'image/png', href: '/favicon-192x192.png', sizes: '192x192' },
       ],
     },
   },
-  ssr: true,
+
+  ssr: false, // Asegúrate de que SSR está habilitado si estás usando SSR
+
   compatibilityDate: '2025-02-13',
 })
